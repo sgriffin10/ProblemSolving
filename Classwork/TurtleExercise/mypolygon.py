e208f534e84a658833d5171962ff47e87133fe4d
sgriffin10 <sgriffin4@babson.edu>
2019-09-17 22:16:09 -0400

    Update mypolygon.py

--- Classwork/TurtleExercise/mypolygon.py
+++ Classwork/TurtleExercise/mypolygon.py
@@ -2,6 +2,7 @@ import turtle
 import math
 
 jack = turtle.Turtle()
+jack.speed(0.5)
 print(jack)
 turtle.mainloop
 
@@ -10,18 +11,18 @@ turtle.mainloop
 #         t.fd(100)
 #         t.lt(90)
 
-# def pentagon(t):
-#     for i in range(5):
-#         t.fd(100)
-#         t.lt(72)
+def pentagon(t):
+    for i in range(4):
+        t.fd(50)
+        t.lt(90)
 
-def circle(t,r):
-    circumference = 2 * math.pi * r
-    n = 50
-    length = circumference / n
-    polygon(t, n, length)
+# def circle(t,r):
+#     circumference = 2 * math.pi * r
+#     n = 50
+#     length = circumference / n
+#     polygon(t, n, length)
 
 # square(jack)
-# pentagon(jack)
-circle (jack, 5)
+pentagon(jack)
+# circle (jack, 5)
 
