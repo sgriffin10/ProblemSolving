75f1fe69579d486786c308d313892825aad9e322
sgriffin10 <sgriffin4@babson.edu>
2019-10-18 22:50:33 -0400

    final_exercises

--- Classwork/Session 12/Exercises/exercises.py
+++ Classwork/Session 12/Exercises/exercises.py
@@ -1,4 +1,4 @@
-#Exercise 2.1
+# Exercise 2.1
 
 # import operator
 # word = input("Enter a word:")
@@ -10,11 +10,11 @@
 # sorted_d = sorted(letter_d.items(), key = operator.itemgetter(1),reverse=True)
 # print(sorted_d)
 
-#Exercise 2.2 
+# Exercise 2.2 & 2.3
 
 ''' Hint: you might want to build a dictionary that maps from a collection of letters to a list of words that can be spelled with those letters. The question is, how can you represent the collection of letters in a way that can be used as a key? '''
 
-anagram_list = ['deltas', 'jesus', 'lasted', 'is', 'slated', 'king']
+anagram_list = ['deltas', 'jesus', 'lasted', 'is', 'slated', 'king', 'deltas', 'desalt', 'lasted', 'salted', 'slated', 'staled','retainers', 'ternaries', 'generating', 'greatening','resmelts', 'smelters', 'termless'] # or ['deltas', 'jesus', 'lasted', 'is', 'slated', 'king', 'deltas'] for testing of 2.2
 
 def anagram(l):
     d = {}  
@@ -30,7 +30,10 @@ def anagram(l):
 if __name__ == '__main__':
     d = anagram(anagram_list)
 
-    for words in d.values():
-        if len(words) > 1:
-            print('Anagrams: {}'.format(', '.join(words)))
+    # sorted_items = sorted(d.items(), key = lambda item: len(item[1]), reverse = True) #for part 2.3
+    # new_dict = dict(sorted_items[:]) #for part 2.3
+    for words in d.values(): #change this between new_dict and d
+        if len(words) > 1: 
+            print('Anagrams: {}'.format(', '.join(words))) 
+            
 
